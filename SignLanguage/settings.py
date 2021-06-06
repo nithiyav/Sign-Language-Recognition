c4f1ed6714fe9f734f089ddeccf44018f43b0512
Satwik Ram <satwikram29@gmail.com>
2021-06-06 11:09:19 +0530

    Db Settings

--- SignLanguage/settings.py
+++ SignLanguage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from secret import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1vnbq$#fh_^18bhzne$r3%&s2+!_@-jbs-u+=c(03*2n-wwlnn'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'SignLanguage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'slr',
+        'PASSWORD': db_password,
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres'
+
     }
 }
 
